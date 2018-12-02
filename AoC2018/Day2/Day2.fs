2113a509e9b8d086e1dafe55bf9bddc4204f53b0
Colin DeClue <cdeclue@rightpoint.com>
2018-12-02 16:27:21 -0600

    Clean up day 2

--- AoC2018/Day2/Day2.fs
+++ AoC2018/Day2/Day2.fs
@@ -1,5 +1,6 @@
 namespace AoC2018
 open System.IO
+open System
 
 module Day2 =
     let readLines (filePath:string) = seq {
@@ -8,10 +9,10 @@ module Day2 =
             yield sr.ReadLine()
     }
 
-    let countChar x = Seq.filter ((=) x) >> Seq.length
+    let countChar (word) (x) =  word |> Seq.filter ((=) x) |> Seq.length
     
-    let acceptWord (word:string) (count:int) = 
-        let counts = word |> Seq.map (fun x -> countChar x word)
+    let acceptWord (count) (word) = 
+        let counts = word |> Seq.map (countChar word)
         counts |> Seq.exists ((=) count)
 
     let acceptWords (word1:string) (word2:string) =
@@ -21,16 +22,13 @@ module Day2 =
             if word1.[i] <> word2.[i] then
                 diff <- diff + 1
                 index <- i
-        if diff = 1 then
-            printfn "%d" index
         diff = 1
 
     let part1 (words:seq<string>) = 
-        let count2 = words |> Seq.filter (fun word -> acceptWord word 2) |> Seq.length
-        let count3 = words |> Seq.filter (fun word -> acceptWord word 3) |> Seq.length
+        let count2 = words |> Seq.filter (acceptWord 2) |> Seq.length
+        let count3 = words |> Seq.filter (acceptWord 3) |> Seq.length
         count2 * count3
 
     let part2 (words:seq<string>) =
-        words |> Seq.filter (fun word -> words |> Seq.exists (fun word2 -> acceptWords word word2))
-
-    
+        let almostMatch = words |> Seq.filter (fun word -> words |> Seq.exists (acceptWords word)) |> Seq.toList
+        Seq.zip almostMatch.[0] almostMatch.[1] |> Seq.filter (fun (f, s) -> f = s) |> Seq.map (fun (f,s) -> f) |> Seq.toArray |> String
